5db90fb4439fc8e57c6bde93b85bcb444d3fe938
Boy Prakasa <boyprakasa@gmail.com>
2026-09-18 13:15:49 +0700

    Load environment variables from .env file in config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * config/config.php
  * Pengaturan umum aplikasi.
@@ -9,5 +10,12 @@
  *   - Jika diletakkan di htdocs/sekolah-app/       -> ubah jadi '/sekolah-app/'
  */
 
-define('APP_NAME', 'Sistem Data Sekolah');
-define('BASE_URL', '/');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+define('APP_NAME', $_ENV['APP_NAME']);
+define('BASE_URL', $_ENV['BASE_URL']);
