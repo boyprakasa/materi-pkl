55673d0818969ee5611de8cf06d7b1209dfa001d
Boy Prakasa <boyprakasa@gmail.com>
2026-09-21 15:50:57 +0700

    Refactor configuration loading: remove dotenv loading from config files and move it to index.php

--- config/config.php
+++ config/config.php
@@ -10,12 +10,5 @@
  *   - Jika diletakkan di htdocs/sekolah-app/       -> ubah jadi '/sekolah-app/'
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
 define('APP_NAME', $_ENV['APP_NAME']);
 define('BASE_URL', $_ENV['BASE_URL']);
--- config/database.php
+++ config/database.php
@@ -9,13 +9,6 @@
  * MySQL/XAMPP/Laragon di komputer kamu.
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__ . '/..');
-$dotenv->load();
-
 $host = $_ENV['DB_HOST'];
 $port = $_ENV['DB_PORT'];
 $database = $_ENV['DB_DATABASE'];
